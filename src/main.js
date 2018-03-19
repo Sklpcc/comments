187e2faa29f08ae327895fea4a251d90b44be787
Sklpcc <christian_sm21@hotmail.com>
2018-03-19 04:41:10 -0500

    Basic apollo-client configuration

--- src/main.js
+++ src/main.js
@@ -1,13 +1,34 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import VueApollo from 'vue-apollo';
 import App from './App';
 
 Vue.config.productionTip = false;
+
+const httpLink = new HttpLink({
+  uri: 'http://localhost:3000/graphql',
+});
+
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+  connectToDevTools: true,
+});
+
+Vue.use(VueApollo);
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+});
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   components: { App },
   template: '<App/>',
+  provide: apolloProvider.provide(),
 });
