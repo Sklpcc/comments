70d5f692efdb3e135a1bdd7251d5954bd9479aad
Sklpcc <christian_sm21@hotmail.com>
2018-03-20 09:58:46 -0500

    Added 255 character restriction, plus UI feedback

--- src/components/CommentInput.vue
+++ src/components/CommentInput.vue
@@ -4,13 +4,25 @@
       <div class="control">
         <textarea
           v-model="content"
+          :class="characterCountClass"
           class="textarea"
           placeholder="Escribe aquí tu comentario!"/>
       </div>
+      <p
+        :class="characterCountClass"
+        class="help">
+        {{ caracterCount }} / {{ maxCaracterCount }}
+        <span
+          v-show="caracterCount > maxCaracterCount"
+          class="is-danger">
+          [Solo se permite hasta 255 caracteres]
+        </span>
+      </p>
     </div>
     <div class="field is-grouped is-grouped-right">
       <div class="control">
         <button
+          :disabled="caracterCount > maxCaracterCount"
           class="button is-primary"
           @click.prevent="addComment">Enviar Comentario</button>
       </div>
@@ -19,12 +31,31 @@
 </template>
 
 <script>
+const maxCaracterCount = 255;
+
 export default {
   name: 'CommentInput',
   data() {
     return {
       content: '',
+      maxCaracterCount,
     };
+  },
+  computed: {
+    caracterCount() {
+      return this.content.length;
+    },
+    characterCountClass() {
+      const safeLimit = this.maxCaracterCount * 0.92;
+      const warningLimit = this.maxCaracterCount;
+
+      if (this.caracterCount <= safeLimit) {
+        return 'is-success';
+      } else if (this.caracterCount <= warningLimit) {
+        return 'is-warning';
+      }
+      return 'is-danger';
+    },
   },
   methods: {
     addComment() {
